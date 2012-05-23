38a08d77f31a155cf367e3404935908bc0443d5f
Akira Matsuda <ronnie@dio.jp>
2012-05-24 06:40:08 +0900

    immediately run generated migration

--- app/controllers/erd/erd_controller.rb
+++ app/controllers/erd/erd_controller.rb
@@ -29,20 +29,25 @@ module Erd
           when 'remove_model'
             generated_migration_file = Erd::Migrator.execute_generate_migration "drop_#{model}"
             gsub_file generated_migration_file, /def up.*  end/m, "def change\n    drop_table :#{model}\n  end"
+            Erd::Migrator.run_migrations :up => generated_migration_file
           when 'rename_model'
             from, to = from.tableize, to.tableize
             generated_migration_file = Erd::Migrator.execute_generate_migration "rename_#{from}_to_#{to}"
             gsub_file generated_migration_file, /def up.*  end/m, "def change\n    rename_table :#{from}, :#{to}\n  end"
+            Erd::Migrator.run_migrations :up => generated_migration_file
           when 'add_column'
             name_and_type = column.scan(/(.*)\((.*?)\)/).first
             name, type = name_and_type[0], name_and_type[1]
-            Erd::Migrator.execute_generate_migration "add_#{name}_to_#{model}", ["#{name}:#{type}"]
+            generated_migration_file = Erd::Migrator.execute_generate_migration "add_#{name}_to_#{model}", ["#{name}:#{type}"]
+            Erd::Migrator.run_migrations :up => generated_migration_file
           when 'rename_column'
             generated_migration_file = Erd::Migrator.execute_generate_migration "rename_#{model}_#{from}_to_#{to}"
             gsub_file generated_migration_file, /def up.*  end/m, "def change\n    rename_column :#{model}, :#{from}, :#{to}\n  end"
+            Erd::Migrator.run_migrations :up => generated_migration_file
           when 'alter_column'
             generated_migration_file = Erd::Migrator.execute_generate_migration "change_#{model}_#{column}_type_to_#{to}"
             gsub_file generated_migration_file, /def up.*  end/m, "def change\n    change_column :#{model}, :#{column}, :#{to}\n  end"
+            Erd::Migrator.run_migrations :up => generated_migration_file
           when 'move'
             json_file = Rails.root.join('tmp', 'erd_positions.json')
             positions = json_file.exist? ? ActiveSupport::JSON.decode(json_file.read) : {}
