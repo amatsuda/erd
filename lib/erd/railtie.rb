1b0341844bc28293e658e8dfe11ebd6ea1ce4648
Akira Matsuda <ronnie@dio.jp>
2013-06-25 16:35:19 +0900

    Initialize the routes a bit earlier (after_initialize  =>  to_prepare)

--- lib/erd/railtie.rb
+++ lib/erd/railtie.rb
@@ -6,12 +6,10 @@ module Erd
   autoload :Migrator, 'erd/migrator'
 
   class Railtie < ::Rails::Railtie #:nodoc:
-    initializer 'erd' do |app|
-      ActiveSupport.on_load(:after_initialize) do
-        if Rails.env.development?
-          Rails.application.routes.append do
-            mount Erd::Engine, :at => '/erd'
-          end
+    config.to_prepare do
+      if Rails.env.development?
+        Rails.application.routes.append do
+          mount Erd::Engine, :at => '/erd'
         end
       end
     end
