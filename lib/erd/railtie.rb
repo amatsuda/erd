b8a14c9795d6fc3d4933476ba17ddb5d62830d33
Akira Matsuda <ronnie@dio.jp>
2013-07-17 20:07:21 +0900

    Revert "Initialize the routes a bit earlier (after_initialize  =>  to_prepare)"
    This reverts commit 1b0341844bc28293e658e8dfe11ebd6ea1ce4648.
    
    reason: this sometimes causes "Invalid route name, already in use: 'erd'"

--- lib/erd/railtie.rb
+++ lib/erd/railtie.rb
@@ -5,10 +5,12 @@ module Erd
   autoload :Migrator, 'erd/migrator'
 
   class Railtie < ::Rails::Railtie #:nodoc:
-    config.to_prepare do
-      if Rails.env.development?
-        Rails.application.routes.append do
-          mount Erd::Engine, :at => '/erd'
+    initializer 'erd' do |app|
+      ActiveSupport.on_load(:after_initialize) do
+        if Rails.env.development?
+          Rails.application.routes.append do
+            mount Erd::Engine, :at => '/erd'
+          end
         end
       end
     end
