903d2a0caf9afc73221c8f7584edbd2b9fe47217
Akira Matsuda <ronnie@dio.jp>
2016-01-23 16:17:04 +0900

    Wait until finding minimum 1 match

--- test/features/erd_test.rb
+++ test/features/erd_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class ErdIndexTest < ActionDispatch::IntegrationTest
   test 'with author and book model' do
     visit '/erd'
-    assert has_content? 'Author'
+    assert has_content? 'Author', minimum: 1
     assert has_content? 'name'
     assert has_content? 'Book'
     assert has_content? 'title'
