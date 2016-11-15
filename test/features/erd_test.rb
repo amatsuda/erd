8f20934baf7f226a3383b789f6f526c4ccf6a592
Akira Matsuda <ronnie@dio.jp>
2016-11-15 15:56:48 +0900

    Make sure that the test app is properly working

--- test/features/erd_test.rb
+++ test/features/erd_test.rb
@@ -3,6 +3,8 @@ require 'test_helper'
 class ErdIndexTest < ActionDispatch::IntegrationTest
   test 'with author and book model' do
     visit '/erd'
+
+    assert_equal 200, page.status_code
     assert has_content? 'Author', minimum: 1
     assert has_content? 'name'
     assert has_content? 'Book'
