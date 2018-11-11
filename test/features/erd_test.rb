4716e880c34d5eac8fe4c260d891ec977eb1ede8
Akira Matsuda <ronnie@dio.jp>
2018-11-12 07:47:41 +0900

    :rocket:

--- test/features/erd_test.rb
+++ test/features/erd_test.rb
@@ -4,7 +4,7 @@ class ErdIndexTest < ActionDispatch::IntegrationTest
   test 'with author and book model' do
     visit '/erd'
 
-    assert has_content? 'Author', minimum: 1
+    assert has_content? 'Author', :minimum => 1
     assert has_content? 'name'
     assert has_content? 'Book'
     assert has_content? 'title'
