34914ca7b89306ffd52c20b2bb149c05941d578c
Akira Matsuda <ronnie@dio.jp>
2018-11-12 07:48:13 +0900

    "wrong number of arguments (2 for 1)" with capybara 2

--- test/features/erd_test.rb
+++ test/features/erd_test.rb
@@ -4,7 +4,11 @@ class ErdIndexTest < ActionDispatch::IntegrationTest
   test 'with author and book model' do
     visit '/erd'
 
-    assert has_content? 'Author', :minimum => 1
+    if Capybara::VERSION > '3'
+      assert has_content? 'Author', :minimum => 1
+    else
+      assert has_content? 'Author'
+    end
     assert has_content? 'name'
     assert has_content? 'Book'
     assert has_content? 'title'
