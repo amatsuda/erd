6ce1a23f631a8883c1e8978495d085b845650c86
Akira Matsuda <ronnie@dio.jp>
2018-11-12 03:45:37 +0900

    We need to run the CI under development env

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
-ENV['RAILS_ENV'] ||= 'development'
+ENV['RAILS_ENV'] = 'development'
 
 # load Rails first
 require 'rails'
