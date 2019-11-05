567a6fcd850d412904978768fd0265fedccb15e1
Akira Matsuda <ronnie@dio.jp>
2019-11-05 11:55:11 +0900

    Merge the initializers into one

--- lib/erd/engine.rb
+++ lib/erd/engine.rb
@@ -4,8 +4,14 @@ module Erd
   class Engine < ::Rails::Engine
     isolate_namespace Erd
 
-    initializer 'erd engine' do |app|
+    initializer 'erd' do |app|
       if Rails.env.development?
+        ActiveSupport.on_load :after_initialize do
+          Rails.application.routes.prepend do
+            mount Erd::Engine, :at => '/erd'
+          end
+        end
+
         app.middleware.insert_before ::ActionDispatch::Static, ::ActionDispatch::Static, root.join('public').to_s
       end
     end
--- lib/erd/railtie.rb
+++ lib/erd/railtie.rb
@@ -6,16 +6,4 @@ require 'erd/engine'
 module Erd
   autoload :Migrator, 'erd/migrator'
   autoload :GenaratorRunner, 'erd/generator_runner'
-
-  class Railtie < ::Rails::Railtie #:nodoc:
-    initializer 'erd' do
-      if Rails.env.development?
-        ActiveSupport.on_load :after_initialize do
-          Rails.application.routes.prepend do
-            mount Erd::Engine, :at => '/erd'
-          end
-        end
-      end
-    end
-  end
 end
