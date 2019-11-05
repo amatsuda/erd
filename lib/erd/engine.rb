478a2c478db3c4e4af78e693c8657ba986a56de2
Akira Matsuda <ronnie@dio.jp>
2019-11-05 11:52:03 +0900

    Serve static assets from the engine

--- lib/erd/engine.rb
+++ lib/erd/engine.rb
@@ -3,5 +3,11 @@
 module Erd
   class Engine < ::Rails::Engine
     isolate_namespace Erd
+
+    initializer 'erd engine' do |app|
+      if Rails.env.development?
+        app.middleware.insert_before ::ActionDispatch::Static, ::ActionDispatch::Static, root.join('public').to_s
+      end
+    end
   end
 end
