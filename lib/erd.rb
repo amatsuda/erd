cbb4587f23dbbbf7ccfbc703e04afc9da762b023
Akira Matsuda <ronnie@dio.jp>
2019-11-05 11:57:06 +0900

    Death to railtie.rb without a Railtie

--- lib/erd.rb
+++ lib/erd.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 require 'erd/version'
-require 'erd/railtie'
+require 'rails'
+require 'erd/engine'
+
+module Erd
+  autoload :Migrator, 'erd/migrator'
+  autoload :GenaratorRunner, 'erd/generator_runner'
+end
--- lib/erd/railtie.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'rails'
-require 'erd/engine'
-
-module Erd
-  autoload :Migrator, 'erd/migrator'
-  autoload :GenaratorRunner, 'erd/generator_runner'
-end
